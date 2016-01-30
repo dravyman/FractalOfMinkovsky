814fe1e50607a885f08b459ff0660b31a71cf1d0
dravyman <drav10@rambler.ru>
2016-01-30 13:05:26 +0300

    Fractal
    Рабочий фрактал

---
+++ FractalOfMinkovsky/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(812, 443);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1ds";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(749, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(163, 4);
+            this.trackBar1.Maximum = 180;
+            this.trackBar1.Minimum = 2;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar1.Size = new System.Drawing.Size(556, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 2;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 510);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ FractalOfMinkovsky/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int speed;
+        List<Point> myPoints = new List<Point>();
+        bool isGo = false;
+        int depth = 0;
+        Thread t;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (t == null || !t.IsAlive)
+            {
+                t = new Thread(changePoints);
+                t.Start();
+                depth++;
+                label1.Text = isGo + "   " + depth;
+            }
+            else if(t.ThreadState == ThreadState.Suspended)
+                t.Resume();
+        }
+         
+        private void changePoints()
+        {
+            for (int i = 0; i < myPoints.Count - 1;)
+            {
+                Point a1 = myPoints[i];
+                Point a2 = myPoints[i + 1];
+                List<Point> clearList = new List<Point>();
+                clearList.Add(a1);
+                clearList.Add(a2);
+                int sizeX = (a2.X - a1.X) / 4;
+                int sizeY = (a2.Y - a1.Y) / 4;
+                if (sizeY == 0)
+                {
+                    myPoints.Insert(i+1,new Point(a1.X + sizeX, a1.Y));
+                    myPoints.Insert(i+2,new Point(a1.X + sizeX, a1.Y - sizeX));
+                    myPoints.Insert(i+3,new Point(a1.X + 2 * sizeX, a1.Y - sizeX));
+                    myPoints.Insert(i+4,new Point(a1.X + 2 * sizeX, a1.Y));
+                    myPoints.Insert(i + 5, new Point(a1.X + 2 * sizeX, a1.Y + sizeX));
+                    myPoints.Insert(i + 6, new Point(a1.X + 3 * sizeX, a1.Y + sizeX));
+                    myPoints.Insert(i+7,new Point(a1.X + 3 * sizeX, a1.Y));
+                }
+                else
+                {
+                    myPoints.Insert(i + 1, new Point(a1.X, a1.Y + sizeY));
+                    myPoints.Insert(i + 2, new Point(a1.X + sizeY, a1.Y + sizeY));
+                    myPoints.Insert(i + 3, new Point(a1.X + sizeY, a1.Y + 2 * sizeY));
+                    myPoints.Insert(i + 4, new Point(a1.X, a1.Y + 2 * sizeY));
+                    myPoints.Insert(i + 5, new Point(a1.X - sizeY, a1.Y + 2 * sizeY));
+                    myPoints.Insert(i + 6, new Point(a1.X - sizeY, a1.Y + 3 * sizeY));
+                    myPoints.Insert(i + 7, new Point(a1.X, a1.Y + 3 * sizeY));
+                }
+                drawFrac(clearList,0,1,Pens.White);
+                drawFrac(myPoints, i, i+8);
+                i += 8;
+                pictureBox1.Invalidate();
+                Thread.Sleep(speed);
+                
+            
+            }
+        }
+
+        private void drawFrac(List<Point> myPoints,int start, int stop, Pen pen = null)
+        {
+            if (pen == null)
+                pen = Pens.Red;
+            Image img = pictureBox1.Image;
+            Graphics gr = Graphics.FromImage(img);
+
+            for (int i = start; i < stop; i++)
+            {
+                gr.DrawLine(pen, myPoints[i], myPoints[i + 1]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isGo)
+            {
+                isGo = false;
+                timer1.Stop();
+                if (t.ThreadState == ThreadState.Running || t.ThreadState == ThreadState.WaitSleepJoin)
+                    t.Suspend();
+            }
+            else
+            {
+                isGo = true;
+                timer1.Start();
+                //MessageBox.Show(Thread.CurrentThread.ManagedThreadId.ToString());
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = isGo + "   " + depth;
+            myPoints.Add(new Point(100, pictureBox1.ClientSize.Height / 2));
+            myPoints.Add(new Point(pictureBox1.ClientSize.Width - 100, pictureBox1.ClientSize.Height / 2));
+            pictureBox1.Image = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+            drawFrac(myPoints,0,1);
+            t = new Thread(changePoints);
+            speed = trackBar1.Maximum;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            isGo = false;
+            pictureBox1.Image = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+            myPoints.Clear();
+            myPoints.Add(new Point(100, pictureBox1.ClientSize.Height / 2));
+            myPoints.Add(new Point(pictureBox1.ClientSize.Width - 100, pictureBox1.ClientSize.Height / 2));
+            depth = 0;
+            isGo = false;
+            drawFrac(myPoints,0,1);
+            label1.Text = isGo + "   " + depth;
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            speed = trackBar1.Maximum - trackBar1.Value; 
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (t != null && t.ThreadState == ThreadState.Suspended)
+            {
+                t.Resume();
+            }
+            t.Abort();
+
+
+        }
+    }
+}
